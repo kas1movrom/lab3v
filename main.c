af8eb0016633f88675603ddc067ffd37c04ed4e3
kas1movrom <romkasrm1985@gmail.com>
2023-03-20 16:53:28 +0300

    add structs table, key_space & item

--- main.c
+++ main.c
@@ -3,6 +3,24 @@
 #include <string.h>
 #include <readline/readline.h>
 
+typedef struct item {
+	unsigned int release;
+	unsigned int key;
+	char *info;
+	struct item *next;
+} item;
+
+typedef struct key_space {
+	int hash;
+	item *list;
+} key_space;
+
+typedef struct table {
+	key_space **arr;
+	int m_size;
+	int c_size;
+} table;
+
 
 int main()
 {
